64385cfa32b2ea35305309000fc554857d723d1d
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2025-07-08 15:54:12 +0700

    update

---
+++ materi5.js
@@ -0,0 +1,24 @@
+// Function Declaration (cara umum)
+// function sapa(nama) {
+//     return `Halo, ${nama}`;
+// }
+
+// Function Expression
+// const sapa = function(nama) {
+//     return `Halo, ${nama}`;
+// }
+
+// Arrow Function (modern & ringkas)
+// const sapa = (nama) => `Halo, ${nama}`;
+
+// console.log(sapa("Kazuhiko"));
+
+// function dengan parameter & return value
+// function hitungTotal(harga, jumlah) {
+//     return harga * jumlah;
+// }
+// let total = hitungTotal(10000,3);
+
+// console.log(`Total harga: ${total}`);
+
+
