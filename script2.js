61a056b0461829981b86c9da9e9a94c10e51f6b4
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2025-09-11 15:45:43 +0700

    Update: MiniProject
    training about DOM and function

--- script2.js
+++ script2.js
@@ -1,18 +1,33 @@
-function belanja(uang) {
-    hargaBarang = 150000;
+// function belanja(uang) {
+//     hargaBarang = 150000;
 
-    if (uang >= hargaBarang) {
-        console.log("Barang bisa dibeli");
+//     if (uang >= hargaBarang) {
+//         console.log("Barang bisa dibeli");
+//     } else {
+//         console.log("Uang tidak cukup");
+//     }
+// }
+
+// belanja(150000);
+
+// let menyapa = document.getElementById("sapa");
+// let btn = document.getElementById("btnSapa");
+
+// btn.addEventListener("click", function () {
+//     menyapa.textContent = "Halo, Selamat datang";
+// })
+
+
+document.getElementById("cekBelanja").addEventListener("click", function () {
+    let belanja = Number(document.getElementById("belanja").value);
+    let uang = Number(document.getElementById("uang").value);
+    let output = document.getElementById("output");
+
+    if (uang <= belanja) {
+        output.textContent = "Uang Tidak Cukup";
+        output.classList.add("output");
     } else {
-        console.log("Uang tidak cukup");
-    }
-}
-
-belanja(150000);
-
-let menyapa = document.getElementById("sapa");
-let btn = document.getElementById("btnSapa");
-
-btn.addEventListener("click", function () {
-    menyapa.textContent = "Halo, Selamat datang";
-})
+        output.textContent = "Uang Cukup Ayo Beli";
+        output.classList.add("output");
+    };
+});
